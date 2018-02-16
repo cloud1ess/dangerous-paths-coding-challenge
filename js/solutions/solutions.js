91badc922c2232531a4ccd9086cd91940be96e21
Tom Walter <waltet05@mc-s083571.home>
2018-02-16 10:38:27 +0000

    Removed my solution from master

--- js/solutions/solutions.js
+++ js/solutions/solutions.js
@@ -1,31 +1,20 @@
 function Solutions () {
 
-  // api.move(dir) dir = up/down/left/right
-  // api.getCellTypeFromOffset({x:0, y:0}) returns 'path/collapser/dissapearing/checkpoint/finish/empty'
-
-  var interval
+  // ------------------------------------------------------------
+  // Game Api
+  // ------------------------------------------------------------
+  // api.move(dir)
+  // api.getCellTypeFromOffset(offset) returns cellType
+  //
+  // dir = up/down/left/right
+  // offset = {x:xOffsetFromPlayerPos, y:yOffsetFromPlayerPos}
+  // cellType = path/finish/empty/collapser/dissapearing/checkpoint
 
   function runSolution (index, api) {
-    if (interval) {
-      window.clearInterval(interval);
-      interval = null;
-    }
 
-    interval = window.setInterval(function () {
-      var route = RouteFinder(api.getCellTypeFromOffset);
-      if (route) {
-        window.clearInterval(interval);
-        interval = null;
-        PerformRoute(route, api.move);
-      }
-    }, 110);
   }
 
   function stopSolution() {
-    if (interval && interval !== 0) {
-      window.clearInterval(interval);
-      interval = null;
-    }
   }
 
   return {
