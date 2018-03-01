9adb3250a774913729350fd2d52411aebc9e7a72
Joel Keers <joel.keers@bbc.co.uk>
2018-03-01 17:22:11 +0000

    add a solution for scenario 1. WIP for scenario 2 along with some ideas to return to.

--- js/solutions/solutions.js
+++ js/solutions/solutions.js
@@ -11,12 +11,96 @@ function Solutions () {
   // Refer to constants.js for <data type>
   // Offset is the relative to the players position
 
-  function runSolution (index, api) {
+  var previousMove = {x:0, y:0};
+  var playerCells = [
+    {x:-1, y:0},
+    {x:0, y:0},
+    {x:1, y:0},
+    {x:-1, y:1},
+    {x:1, y:1},
+    {x:0, y:1},
+    {x:1, y:-1},
+    {x:0, y:-1}]
 
+
+  function runSolution (index, api) {
+    switch (index) {
+      case 0: 
+        simplePath(api);
+        break;
+      case 1:
+        multiPath(api);
+        break;
+      default:
+        break;
+    }
   }
 
   function stopSolution() {
   }
+
+  function simplePath (api) {
+    api.move(DIRS.down);
+    api.move(DIRS.down);
+    api.move(DIRS.right);
+    api.move(DIRS.right);
+    api.move(DIRS.up);
+    api.move(DIRS.right);
+    api.move(DIRS.right);
+    api.move(DIRS.down);
+    api.move(DIRS.down);
+  }
+
+  function multiPath (api) {
+    var running = true;
+    while (running) {
+      var move = validDirection(api)
+      api.move(move);
+      previousMove = move;  
+      if (getNextOutcomes(api).includes(OUTCOMES.finish)) {
+        running = false;
+      }
+    }
+  }
+
+  function getNextOutcomes(api) {
+
+  }
+
+  function nextPathDirection (api) {
+    var directions = [];
+    playerCells.forEach(function (cell){
+      if (api.getCellTypeFromOffset(cell) === CELL_TYPES.path) {
+        directions.push(cell);
+      }
+    });
+    return directions;
+  } 
+
+  function validDirection (api) {
+    var pathDirections = nextPathDirection(api);
+    pathDirections.forEach(function (cell) {
+      if (cell.x !== previousMove.x && cell.y !== previousMove.y) {
+        return cell;
+      }
+    });
+  }
+
+  // TODO: will be needed to check whether a path is valid to travel.
+  // Correction: This can be thought of a shortest path problem. Graph algorithms ahoy! Check out some maze running algorithms too. 
+  // Depth first search, breadth first seach, greedy algos etc.
+
+  function isPathTraversable () {}
+
+  function pathDirection() {}
+
+  function isUp () {}
+
+  function isDown () {}
+
+  function isRight () {}
+
+  function isLeft () {}
 
   return {
     runSolution: runSolution,
