be8b1f0ffb498563ba0ae8ca676c83443292b0c0
Rob Harper <rob.harper@bbc.co.uk>
2018-03-06 16:45:51 +0000

    Now working for scenario 2 (big refactor)

--- js/solutions/solutions.js
+++ js/solutions/solutions.js
@@ -17,31 +17,44 @@ function Solutions () {
   possibleMoves[DIRS.up] = {x:0, y:-1};
   possibleMoves[DIRS.left] = {x:-1, y:0};
 
-  opposites = {}
+  const opposites = {}
   opposites[DIRS.down] = DIRS.up;
   opposites[DIRS.right] = DIRS.left;
   opposites[DIRS.up] = DIRS.down;
   opposites[DIRS.left] = DIRS.right;
 
-  function getNextMove(api, lastMove) {
-    var nextMove
-    for (var move in possibleMoves) {
-      if (move !== opposites[lastMove]) {
-        if (api.getCellTypeFromOffset(possibleMoves[move]) === CELL_TYPES.path) return move
-      }
-    }
+  function getNextMove(api, position, history) {
+    const lastMove = history.length && history[history.length - 1].move;
+    const movesAlreadyTried = history.filter(h => h.pos.x === position.x && h.pos.y === position.y).map(h => h.move)
+    var moveList = Object.keys(possibleMoves)
+      .filter(m => m !== opposites[lastMove])
+      .filter(m => movesAlreadyTried.indexOf(m) === -1)
+      .concat(movesAlreadyTried)
+      .concat(opposites[lastMove])
+    return moveList.map((move) => { return {move: move, type: api.getCellTypeFromOffset(possibleMoves[move])}})
+      .filter(mt => mt.type === CELL_TYPES.path)[0].move
+  }
+
+  function getNewPosition(currentPosition, move) {
+    var moves = {}
+    moves[DIRS.down] = (pos) => { return {x: pos.x, y: pos.y + 1}}
+    moves[DIRS.up] = (pos) => { return {x: pos.x, y: pos.y - 1}}
+    moves[DIRS.left] = (pos) => { return {x: pos.x - 1, y: pos.y}}
+    moves[DIRS.right] = (pos) => { return {x: pos.x + 1, y: pos.y}}
+    return moves[move](currentPosition)
   }
 
   function runSolution (index, api) {
+    const history = []
     let outcome;
-    let count = 0;
-    let lastMove;
-    while(outcome !== OUTCOMES.finish && count < 100){
-      const move = getNextMove(api, lastMove);
+    let position = {x: 0, y: 0};
+
+    while(outcome !== OUTCOMES.finish){
+      const move = getNextMove(api, position, history);
       outcome = api.getOutcomeFromOffset(possibleMoves[move]);
       api.move(move);
-      lastMove = move;
-      count++;
+      history.push({pos: position, move});
+      position = getNewPosition(position, move);
     }
   }
 
