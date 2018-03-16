55500d485e99ec5d9c522cdfe09e481414b198f4
Joshua Malkinson <joshua.malkinson@bbc.co.uk>
2018-03-16 14:38:37 +0000

    Add semicolons and line breaks

--- js/constants.js
+++ js/constants.js
@@ -3,21 +3,24 @@ var DIRS = {
   down: 'down',
   left: 'left',
   right: 'right'
-}
+};
+
 var CELL_TYPES = {
   path: 'path',
   disappearing: 'disappearing',
   collapsing: 'collapsing',
   checkpoints: 'checkpoints'
-}
+};
+
 var OUTCOMES = {
   survive: 'survive',
   die: 'die',
   finish: 'finish'
-}
+};
+
 var OFFSETS = {
   up: { x: 0, y: -1 },
   right: { x: 1, y: 0 },
   down: { x: 0, y: 1 },
   left: { x: -1, y: 0 }
-}
+};
